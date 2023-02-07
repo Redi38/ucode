b1f5835639e94be539f73c69a56a754dd44d3a3a
Redi38 <alecspetrow88@gmail.com>
2023-02-07 16:25:54 +0200

    tasks

---
+++ Sprint08/t01/duplicate.h
@@ -0,0 +1,16 @@
+#ifndef DUPLICATE_H
+#define DIPLICATE_H
+
+#include <stdlib.h>
+
+typedef struct s_intarr
+{
+	int *arr;
+	int size;
+}		t_intarr;
+
+t_intarr *mx_del_dup_sarr(t_intarr *);
+
+t_intarr *mx_copy_int_arr(const t_intarr *);
+
+#endif
